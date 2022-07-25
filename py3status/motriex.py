d5ee8d54634642753c1898e43c843fc5cf6d56c0
Andrey_Shleger <Andrey_Shleger@epam.com>
2022-07-25 23:33:32 +0300

    show currency in status bar

--- py3status/motriex.py
+++ py3status/motriex.py
@@ -16,30 +16,40 @@ https://programtalk.com/vs2/python/13516/py3status/py3status/modules/volume_stat
 
 
 import subprocess
+# import json
+import datetime
 
 
 class Py3status:
     """Placeholder for running status command."""
 
     format = 'Number {number}'
-    cache_timeout = 10
+    cache_timeout = 30
 
     def run(self):
         """Runner method."""
-        result = subprocess.run(['date', '+%S'],
+        now = datetime.datetime.now()
+        if now.hour > 19 or now.hour < 10:
+            return {
+                'full_text': "-",
+                'color': self.py3.COLOR_HIGH,
+                'cached_until': self.py3.time_in(self.cache_timeout)
+            }
+        # result = subprocess.run(['date', '+%S'],
+        result = subprocess.run(['/home/saa/.config/i3/py3status/motriex-proto-lens'],
                                 capture_output=True,
                                 text=True)
 
-        number = int(result.stdout.strip())
-        full_text = self.py3.safe_format(self.format, {'number': number})
+        number = float(result.stdout.strip())
+        # full_text = self.py3.safe_format(self.format, {'number': number})
 
-        if number < 30:
+        if number < 60.0:
             color = self.py3.COLOR_LOW
         else:
             color = self.py3.COLOR_HIGH
 
         return {
-            'full_text': full_text,
+            'full_text': str(number),
             'color': color,
             'cached_until': self.py3.time_in(self.cache_timeout)
         }
@@ -47,3 +57,15 @@ class Py3status:
     def on_click(self, event):
         """Exec run function onClick."""
         pass
+
+    # def prn():
+    #     """Exec debug shell command."""
+    #     result = subprocess.run(['./motriex-proto-lens'],
+    #                             capture_output=True,
+    #                             text=True)
+    #     # jsonData = json.loads(json.loads(result.stdout.strip()))
+    #     # print(jsonData['code'])
+    #     number2 = float(result.stdout.strip())
+    #     return {
+    #         "out": number2
+    #     }
